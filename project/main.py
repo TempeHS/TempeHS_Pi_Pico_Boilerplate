fc43283910cf6fe90e989c35ded6eacadfddea4d
Ben Jones <benpaddlejones@outlook.com>
2025-01-12 23:19:25 +1100

    brick fix attempt

--- project/main.py
+++ project/main.py
@@ -1,5 +1,21 @@
-import sys
+import machine, sys
 
 sys.path.append("/libs")
 sys.path.append("/py_scripts")
-import v01  # Change this as you create more versions
+
+import v01 as pi_pico_project  # Change this as you create more versions
+
+try:
+    pi_pico_project.setup()
+    pi_pico_project.loop()
+except KeyboardInterrupt:
+    print("Exiting main.py")
+except Exception as e:
+    print("Fatal error in main:")
+    sys.print_exception(e)
+
+# Following a normal Exception or main() exiting, reset the board.
+# Following a non-Exception error such as KeyboardInterrupt (Ctrl-C),
+# this code will drop to a REPL. Place machine.reset() in a finally
+# block to always reset, instead.
+machine.reset()
