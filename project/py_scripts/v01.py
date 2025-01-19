bf0985f407bf10eab06526036f08b0faadb1a87f
Ben Jones <benpaddlejones@outlook.com>
2025-01-19 20:53:37 +1100

    improve error feedback

--- project/py_scripts/v01.py
+++ project/py_scripts/v01.py
@@ -1,12 +1,10 @@
 from machine import Pin
 from utime import sleep
 
-stop_pin = Pin(4, Pin.IN, Pin.PULL_UP)
 
 pin = Pin("LED", Pin.OUT)
 
-
-while stop_pin.value():
+while True:
     pin.toggle()
     sleep(1)  # sleep 1sec
     print("LED is ON" if pin.value() else "LED is OFF")
